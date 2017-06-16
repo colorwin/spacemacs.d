d068fca3113304f30b427fa4225e5d88785e8ce6
colorwin <b504008585@163.com>
2017-06-17 00:50:36 +0800

    2017年 06月 17日 星期六 00:50:36 CST

--- init.el
+++ init.el
@@ -329,6 +329,28 @@ you should place your code here."
     (add-to-list 'auto-mode-alist '("\\.wxss$" . css-mode))
     (add-to-list 'auto-mode-alist '("\\.vue$" . web-mode))
 
+    ;; copy to system clipboard
+(case system-type
+  ('darwin (unless window-system
+             (setq interprogram-cut-function
+                   (lambda (text &optional push)
+                     (let* ((process-connection-type nil)
+                            (pbproxy (start-process "pbcopy" "pbcopy" "/usr/bin/pbcopy")))
+                       (process-send-string pbproxy text)
+                       (process-send-eof pbproxy))))))
+  ('gnu/linux (progn
+                (setq x-select-enable-clipboard t)
+                (defun xsel-cut-function (text &optional push)
+                  (with-temp-buffer
+                    (insert text)
+                    (call-process-region (point-min) (point-max) "xsel" nil 0 nil "--clipboard" "--input")))
+                (defun xsel-paste-function()
+
+                  (let ((xsel-output (shell-command-to-string "xsel --clipboard --output")))
+                    (unless (string= (car kill-ring) xsel-output)
+                      xsel-output )))
+                (setq interprogram-cut-function 'xsel-cut-function)
+                (setq interprogram-paste-function 'xsel-paste-function))))
   )
 (setq custom-file (expand-file-name "custom.el" dotspacemacs-directory))
 
