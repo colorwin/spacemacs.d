ee86eec275b8b3c87851ee904b0c9d3b44160525
colorwin <b504008585@163.com>
2017-07-12 15:14:31 +0800

    add engine search key

--- layers/zilongshanren/keybindings.el
+++ layers/zilongshanren/keybindings.el
@@ -217,3 +217,32 @@
   )
 
 (spacemacs/set-leader-keys "sw" 'colorwin/swiper)
+
+(defun prelude-search (query-url prompt)
+  "Open the search url constructed with the QUERY-URL.
+PROMPT sets the `read-string prompt."
+  (browse-url
+   (concat query-url
+	   (url-hexify-string
+	    (if mark-active
+		(buffer-substring (region-beginning) (region-end))
+	      (read-string prompt))))))
+
+(defmacro prelude-install-search-engine (search-engine-name search-engine-url search-engine-prompt)                   ; #1
+  "Given some information regarding a search engine, install the interactive command to search through them"
+  `(defun ,(intern (format "prelude-%s" search-engine-name)) ()                                                       ; #2
+       ,(format "Search %s with a query or region if any." search-engine-name)                                        ; #3
+       (interactive)
+       (prelude-search ,search-engine-url ,search-engine-prompt)))                                                    ; #4
+
+(prelude-install-search-engine "google"     "http://www.google.com/search?q="              "Google: ")                ; #5
+(prelude-install-search-engine "youtube"    "http://www.youtube.com/results?search_query=" "Search YouTube: ")
+(prelude-install-search-engine "github"     "https://github.com/search?q="                 "Search GitHub: ")
+(prelude-install-search-engine "duckduckgo" "https://duckduckgo.com/?t=lm&q="              "Search DuckDuckGo: ")
+
+
+
+(spacemacs/declare-prefix "E" "engine")
+(spacemacs/set-leader-keys "Eg" 'engine/search-google)
+(spacemacs/set-leader-keys "EG" 'engine/search-github)
+(spacemacs/set-leader-keys "Ew" 'engine/search-wikipedia)
